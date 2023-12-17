9d5276ee89f1fcd10eeee04a41b1f2d905c84496
tunalad <28594843+tunalad@users.noreply.github.com>
2023-12-17 21:38:24 +0100

    made vue icon clickable
    this also saves the score, though just in the variable atm

---
+++ src/components/Cookie.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref } from "vue";
+
+const gameState = ref({
+    clicks: 0,
+    autoClick: 0,
+});
+
+function cookieClicked(e) {
+    // score
+    gameState.value.clicks += 1;
+
+    // style
+    e.target.classList.add("no-hover");
+    setTimeout(() => {
+        e.target.classList.remove("no-hover");
+    }, 100);
+}
+</script>
+
+<template>
+    <div>
+        <img
+            ref="vue-cookie"
+            src="../assets/vue.svg"
+            class="logo vue"
+            alt="Vue logo"
+            @click="cookieClicked"
+        />
+        <p>{{ gameState.clicks }} projects</p>
+        <p>{{ gameState.autoClick }} per second</p>
+    </div>
+</template>
+
+<style scoped>
+.logo {
+    height: 12em;
+    padding: 1.5em;
+    will-change: filter;
+    transition:
+        filter 100ms,
+        transform 100ms;
+}
+.logo:not(.no-hover):hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+    cursor: pointer;
+    transform: scale(1.2);
+}
+
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
